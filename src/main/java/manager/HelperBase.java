bdeafae5087879b543fef27d8b547adeaa4bff6d
webinartelran <webinartelran@gmail.com>
2023-08-16 00:02:25 +0300

    CW24

---
+++ src/main/java/manager/HelperBase.java
@@ -0,0 +1,29 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public interface HelperBase extends AppManager{
+
+    default void click(By locator){
+        wd.findElement(locator).click();
+    }
+
+    default void type(By locator, String text){
+        WebElement element = wd.findElement(locator);
+        element.click();
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    default void pause(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
